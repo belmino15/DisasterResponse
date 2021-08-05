cc16218b356fd8ff51ea64fed7070017866763d4
Lucas Belmino <lucas.belmino@hotmail.com>
2021-08-04 22:38:26 -0300

    rebuilding project

---
+++ data/process_data.py
@@ -0,0 +1,88 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+from numpy import nan
+
+# python data/process_data.py data/messages.csv data/categories.csv data/DisasterResponse.db
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = pd.merge(messages, categories, how= 'inner', on= 'id')
+
+    return df
+
+
+def clean_data(df):
+    categories = df.categories.str.split(';', expand= True)
+
+    row = categories.head(1)
+    category_colnames = [r.split('-')[0] for r in row.values[0]]
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.split('-', expand= True)[1]
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    del df['categories']
+
+    df = pd.concat([df, categories], axis= 1)
+
+    df = df.drop_duplicates()
+
+    df = df[df.related != 2]
+    df = df[df.message != '#NAME?']
+
+    duplicated_messages = list(df[df.message.duplicated()].message.unique())
+
+    for message in duplicated_messages:
+        # create new row joining the information of the duplicated row (same message) 
+        new_row = df[df.message == message].fillna(-1).groupby(['id', 'message', 'original', 'genre']).any().astype(int).reset_index()
+        new_row.original = nan
+
+        # delete duplicated rows (same message)
+        df = df[df.message != message]
+        
+        # concat the dataframe and the new_row
+        df = pd.concat([df, new_row])
+
+    return df
+
+
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('T_DisasterResponse', engine, if_exists= 'replace', index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ model/train_classifier.py
@@ -0,0 +1,109 @@
+# import packages
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['punkt', 'wordnet'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+
+import pickle
+from functools import partial
+
+# python model/train_classifier.py data/DisasterResponse.db model/model.pkl
+
+def load_data(data_file):
+    # read in file
+    path = data_file
+
+    engine = create_engine('sqlite:///' + path)
+    df = pd.read_sql_table(table_name= 'T_DisasterResponse', con= engine)
+
+    # define features and label arrays
+
+    X = df['message']
+    y = df.drop(columns= ['original', 'id', 'message', 'genre'])
+
+    return X, y
+
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+def build_model():
+
+    # text processing and model pipeline
+
+    pipeline = Pipeline([
+            ('vect', CountVectorizer(tokenizer= partial(tokenize))),
+            ('tfidf', TfidfTransformer()),
+            ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ])
+
+    # define parameters for GridSearchCV
+
+    parameters = {
+        'clf__estimator__n_estimators': [50, 100],
+        'clf__estimator__min_samples_leaf': [5, 10]
+        }
+
+    # create gridsearch object and return as final model pipeline
+
+    model_pipeline = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1, verbose= 2)
+
+    return model_pipeline
+
+
+def train(X, y, model):
+    # train test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42)
+
+    # fit model
+    model.fit(X_train, y_train)
+
+    # output model test results
+    y_pred_improved = model.predict(X_test)
+    print(classification_report(y_test.values, y_pred_improved, target_names= y.columns, zero_division= False))
+    
+    return model
+
+
+def export_model(model, model_path):
+    # Export model as a pickle file
+
+    pickle.dump(model, open(model_path, 'wb'))
+
+def run_pipeline(data_file, model_path):
+    X, y = load_data(data_file)  # run ETL pipeline
+    model = build_model()  # build model pipeline
+    model = train(X, y, model)  # train model pipeline
+    export_model(model, model_path)  # save model
+
+
+if __name__ == '__main__':
+    # get filename of dataset
+    #data_file = sys.argv[1]
+    #model_path = sys.argv[2]
+    
+    data_file = 'data/DisasterResponse.db'
+    model_path = 'model/model.pkl'
+    run_pipeline(data_file, model_path)  # run data pipeline
